4e7bff54be955f82dc8a6c3e414148363d771a3f
Jake Bladt <jake@jakebladt.com>
2016-05-31 11:54:29 -0400

    combine paths

--- scrub.py
+++ scrub.py
@@ -1,8 +1,11 @@
+import os
 import sys
 
-target = ''
+gallery_root = os.environ['GALLERY_ROOT']
 
 if len(sys.argv) > 1:
-  target = sys.argv[1]
-
-
+  gallery_path = os.path.join(gallery_root, sys.argv[1])
+  print(gallery_path)
+else:
+  "loop through all galleries"
+  pass
