4c568ac3106a828c161dbe7f0961dc9c49a8f225
Jake Bladt <jake@jakebladt.com>
2016-06-01 22:44:17 -0400

    count files

--- scrub.py
+++ scrub.py
@@ -1,11 +1,16 @@
 import os
 import sys
 
+from file_finder import FileFinder
+
 gallery_root = os.environ['GALLERY_ROOT']
 
 if len(sys.argv) > 1:
   gallery_path = os.path.join(gallery_root, sys.argv[1])
   print(gallery_path)
+  finder = FileFinder(gallery_path)
+  files = finder.find_files('*.jpg')
+  print('found {} file(s).'.format(len.files))
 else:
   "loop through all galleries"
   pass
