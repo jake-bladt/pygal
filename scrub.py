d94411d385430bd6455a1ad60204b1364341cbee
Jake Bladt <jake@jakebladt.com>
2016-06-03 12:46:51 -0400

    +subject

--- scrub.py
+++ scrub.py
@@ -6,6 +6,7 @@ from file_finder import FileFinder
 gallery_root = os.environ['GALLERY_ROOT']
 
 if len(sys.argv) > 1:
+  subject = sys.argv[1]
   gallery_path = os.path.join(gallery_root, sys.argv[1])
   print(gallery_path)
   finder = FileFinder(gallery_path)
