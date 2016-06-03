4223956bf605bb0889b517ccbf12cacc3c397d7b
Jake Bladt <jake@jakebladt.com>
2016-06-03 12:58:18 -0400

    get simple name

--- image_name_factory.py
+++ image_name_factory.py
@@ -1,3 +1,6 @@
 class ImageNameFactory:
-  def get_canonical_image_names(subject, count):
-  
+  def get_canonical_image_names(self, subject, count):
+    seeds = range(1, count + 1)
+
+  def get_simple_image_name(self, subject, ordinal):
+    return "{}{:03d}.jpg".format(subject, ordinal)
