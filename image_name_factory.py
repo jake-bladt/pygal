3999dbb46ea40378011f613e805b7b3671c520a1
Jake Bladt <jake@jakebladt.com>
2016-06-03 14:14:09 -0400

    return list

--- image_name_factory.py
+++ image_name_factory.py
@@ -1,11 +1,12 @@
 class ImageNameFactory:
   def get_canonical_image_names(self, subject, count):
     seeds = range(1, count + 1)
+    return [get_simple_image_name(s) if count < 1000 else get_volumetric_image_name(s) for s in seeds]
 
   def get_simple_image_name(self, subject, ordinal):
     return "{}{:03d}.jpg".format(subject, ordinal)
 
-  def get_volumetric_image_names(self, subject, ordinal):
+  def get_volumetric_image_name(self, subject, ordinal):
     vol = (ordinal // 1000) + 1
     return "vol{:03d}/{}{:03d}.jpg".format(vol, subject, ordinal)
   
