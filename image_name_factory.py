238197ba389cde375147c151928a7ea647ff2086
Jake Bladt <jake@jakebladt.com>
2016-06-03 13:03:09 -0400

    get volumetric name

--- image_name_factory.py
+++ image_name_factory.py
@@ -4,3 +4,8 @@ class ImageNameFactory:
 
   def get_simple_image_name(self, subject, ordinal):
     return "{}{:03d}.jpg".format(subject, ordinal)
+
+  def get_volumetric_image_names(self, subject, ordinal):
+    vol = (ordinal // 1000) + 1
+    return "vol{:03d}/{}{:03d}.jpg".format(vol, subject, ordinal)
+  
