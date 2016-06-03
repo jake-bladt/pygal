a37256df46773335db15085137f4ee87a8698a0b
Jake Bladt <jake@jakebladt.com>
2016-06-03 14:25:48 -0400

    test simple name

--- scrub.py
+++ scrub.py
@@ -2,12 +2,13 @@ import os
 import sys
 
 from file_finder import FileFinder
+from image_name_factory import ImageNameFactory
 
 gallery_root = os.environ['GALLERY_ROOT']
 
 if len(sys.argv) > 1:
   subject = sys.argv[1]
-  gallery_path = os.path.join(gallery_root, sys.argv[1])
+  gallery_path = os.path.join(gallery_root, subject)
   print(gallery_path)
   finder = FileFinder(gallery_path)
   files = finder.get_files('*.jpg')
